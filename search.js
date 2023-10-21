5476e39864a86a002c7e64d860a91da682e52f1a
eunji624 <allrkatk@naver.com>
2023-10-21 16:05:27 +0900

    [fix] : 검색기능 수정 보완

---
+++ search.js
@@ -0,0 +1,22 @@
+//인풋값 가져오는 함수
+function search() {
+  if (num === 1) {
+    temp = "";
+  }
+  console.log(searchTotal);
+  let inputVal = input.value;
+  //한글 안깨지게 encodeURI()
+  inputVal = encodeURI(inputVal);
+  searchUrl = `https://api.themoviedb.org/3/search/movie?query=${inputVal}&include_adult=false&language=ko-KR&page=${num}`;
+
+  isSearch = true;
+  //검색결과 가져올 전체 데이터
+  fetch(searchUrl, options)
+    .then((response) => response.json())
+    .then(function (response) {
+      searchResult = response.results;
+      searchTotal = response.total_pages;
+      appendFunc(searchResult);
+    })
+    .catch((err) => console.error(err));
+}
